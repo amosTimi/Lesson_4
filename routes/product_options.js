c4fd931f12a063a9d4f4e947a0ff8e62ae6545da
amosTimi <timilehinamos@gmail.com>
2021-01-26 18:29:36 +0100

    update 2

--- routes/product_options.js
+++ routes/product_options.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-// Require index controller.
 var product_optionsController = require('../controllers/product_optionsController');
 
-// when users visit /product_options go to product_options controller
 router.get('/product_options', product_optionsController.productOptions_get);
 
 module.exports = router;
